45ea38ce53fa922e48ec88c843f76f3ba809015b
Henning Redestig <henning.red@gmail.com>
2013-09-01 21:07:09 +0200

    packages in own file

---
+++ emacs/emacs-init-packages.el
@@ -0,0 +1,23 @@
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . 
+								   "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  (unless package-archive-contents
+	(package-refresh-contents))
+  (setq package-load-list '(all))     
+  (unless (package-installed-p 'org)  
+	(package-install 'org))
+  (unless (package-installed-p 'auctex)  
+	(package-install 'auctex))
+  (unless (package-installed-p 'markdown-mode)  
+	(package-install 'markdown-mode))
+  (unless (package-installed-p 'yasnippet)  
+	(package-install 'yasnippet))
+  (unless (package-installed-p 'iedit)  
+	(package-install 'iedit))
+  (unless (package-installed-p 'elscreen)  
+	(package-install 'elscreen))
+  (package-initialize)
+  )
+
--- emacs/emacs-init.el
+++ emacs/emacs-init.el
@@ -1,5 +1,6 @@
 (load-file "~/.emacs.d/emacs-init-basic.el")
 (load-file "~/.emacs.d/emacs-init-keys.el")
+(load-file "~/.emacs.d/emacs-init-packages.el")
 (load-file "~/.emacs.d/emacs-init-local.el")
 (load-file "~/.emacs.d/emacs-init-modes-0.el")
 
